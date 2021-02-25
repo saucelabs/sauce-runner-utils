bcec3eccaf3aa40a6f4ccbed1bfe5992e541671a
Félix P <felix@saucelabs.com>
2021-02-25 14:22:21 -0800

    Update tests

--- tests/unit/src/saucectl.spec.js
+++ tests/unit/src/saucectl.spec.js
@@ -9,46 +9,46 @@ describe('saucectl', function () {
 
   it('should set value in output', function () {
     mock({
-      [saucectl.OUTPUT_FILE_PATH]: '{}',
+      [saucectl.DEFAULT_OUTPUT_FILEPATH]: '{}',
     });
 
-    saucectl.exportValue({ jobDetailsUrl: 'myurl' });
+    saucectl.exportValue(saucectl.DEFAULT_OUTPUT_FILEPATH, { jobDetailsUrl: 'myurl' });
 
-    const data = JSON.parse(fs.readFileSync(saucectl.OUTPUT_FILE_PATH));
+    const data = JSON.parse(fs.readFileSync(saucectl.DEFAULT_OUTPUT_FILEPATH));
     expect(data.jobDetailsUrl).toBe('myurl');
   });
 
   it('should overwrite value in output', function () {
     mock({
-      [saucectl.OUTPUT_FILE_PATH]: '{"previousField":"present"}',
+      [saucectl.DEFAULT_OUTPUT_FILEPATH]: '{"previousField":"present"}',
     });
 
-    saucectl.exportValue({ jobDetailsUrl: 'myurl' });
+    saucectl.exportValue(saucectl.DEFAULT_OUTPUT_FILEPATH, { jobDetailsUrl: 'myurl' });
 
-    const data = JSON.parse(fs.readFileSync(saucectl.OUTPUT_FILE_PATH));
+    const data = JSON.parse(fs.readFileSync(saucectl.DEFAULT_OUTPUT_FILEPATH));
     expect(data.jobDetailsUrl).toBe('myurl');
     expect(data.previousField).toBeUndefined();
   });
 
   it('should append value in output when empty', function () {
     mock({
-      [saucectl.OUTPUT_FILE_PATH]: '{}',
+      [saucectl.DEFAULT_OUTPUT_FILEPATH]: '{}',
     });
 
-    saucectl.updateExportedValue({ jobDetailsUrl: 'myurl' });
+    saucectl.updateExportedValue(saucectl.DEFAULT_OUTPUT_FILEPATH, { jobDetailsUrl: 'myurl' });
 
-    const data = JSON.parse(fs.readFileSync(saucectl.OUTPUT_FILE_PATH));
+    const data = JSON.parse(fs.readFileSync(saucectl.DEFAULT_OUTPUT_FILEPATH));
     expect(data.jobDetailsUrl).toBe('myurl');
   });
 
   it('should overwrite value in output', function () {
     mock({
-      [saucectl.OUTPUT_FILE_PATH]: '{"previousField":"present"}',
+      [saucectl.DEFAULT_OUTPUT_FILEPATH]: '{"previousField":"present"}',
     });
 
-    saucectl.updateExportedValue({ jobDetailsUrl: 'myurl' });
+    saucectl.updateExportedValue(saucectl.DEFAULT_OUTPUT_FILEPATH, { jobDetailsUrl: 'myurl' });
 
-    const data = JSON.parse(fs.readFileSync(saucectl.OUTPUT_FILE_PATH));
+    const data = JSON.parse(fs.readFileSync(saucectl.DEFAULT_OUTPUT_FILEPATH));
     expect(data.jobDetailsUrl).toBe('myurl');
     expect(data.previousField).toBe('present');
   });
