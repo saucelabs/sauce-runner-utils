44596427ade670f6a1a698f4dcfb576883eda0cd
Felix P <felix@saucelabs.com>
2023-03-01 16:30:41 +0100

    Rely on npm binary, not API

--- src/npm.ts
+++ src/npm.ts
@@ -1,23 +1,85 @@
-import util from 'util';
-import npm from 'npm';
+import { spawn } from 'child_process';
+import { lstat, rename, rm, writeFile } from 'fs/promises';
+
+const temporarilyMovedFiles: {[key: string]: string} = {
+  'package.json': `.package.json-${process.pid}`,
+  'package-lock.json': `.package-lock.json-${process.pid}`,
+};
 
 export default class NPM {
-  public static async install (...args: string[]) {
-    const npmInstall = util.promisify(npm.commands.install);
-    await npmInstall(args);
+  static async removePackageJson () {
+    for (const [name, replacement] of Object.entries(temporarilyMovedFiles)) {
+      try {
+        // Note: we don't need the return value.
+        // lstat is only used to check that the file exists.
+        await lstat(name);
+        await rename(name, replacement);
+      } catch (e) {
+        console.debug(`no ${name} to archive`);
+      }
+    }
   }
 
-  public static async load (cfg: {[key: string]: object | string | number | boolean | null }) {
-    await new Promise((resolve) => {
-      npm.load(cfg, () => {
-        resolve(null);
+  static async restorePackageJson () {
+    for (const [name, replacement] of Object.entries(temporarilyMovedFiles)) {
+      try {
+        // Note: we don't need the return value.
+        // lstat is only used to check that the file exists.
+        await lstat(replacement);
+        await rename(replacement, name);
+      } catch (e) {
+        console.debug(`no ${name} was archived`);
+      }
+    }
+  }
+
+  public static async install (pkg: {[key: string]: string}) {
+    await this.removePackageJson();
+    await writeFile('package.json', JSON.stringify({
+      dependencies: pkg,
+    }));
+
+    const p = spawn('npm', ['install']);
+    p.stdout.pipe(process.stdout);
+    p.stderr.pipe(process.stderr);
+
+    const exitPromise = new Promise((resolve) => {
+      p.on('exit', (exitCode) => {
+        resolve(exitCode);
+      });
+    });
+
+    const exitCode = await exitPromise;
+
+    await rm('package.json', { force: true });
+    await this.restorePackageJson();
+
+    return exitCode;
+  }
+
+  public static configure (cfg: {[key: string]: object | string | number | boolean | null }): Promise<number | null> {
+    return new Promise((resolve) => {
+      const args = Object.entries(cfg).map((k, v) => `${k}=${v}`);
+      // FIXME: fix path to NPM
+      const p = spawn('npm', args);
+      p.stdout.pipe(process.stdout);
+      p.stderr.pipe(process.stderr);
+      p.on('exit', (exitCode) => {
+        resolve(exitCode);
       });
     });
   }
 
-  public static async rebuild (...args: string[]) {
-    const npmRebuild = util.promisify(npm.commands.rebuild);
-    await npmRebuild(args);
+  public static rebuild (...args: string[]): Promise<number | null> {
+    return new Promise((resolve) => {
+      // FIXME: fix path to NPM
+      const p = spawn('npm', ['rebuild', ...args]);
+      p.stdout.pipe(process.stdout);
+      p.stderr.pipe(process.stderr);
+      p.on('exit', (exitCode) => {
+        resolve(exitCode);
+      });
+    });
   }
 }
 
